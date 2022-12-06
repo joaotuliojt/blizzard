7a63a4d56e775c8f6cf3674f84b6860f3f8ef999
joaotuliojt <joaotuliosoares@hotmail.com>
2022-12-06 02:09:21 -0300

    feat: create games section

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['api.brchallenges.com'],
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.brchallenges.com',
+        port: '',
+        pathname: '/api/blizzard/games/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/components/containers/Download/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from './style'
+
+export function Download() {
+  return (
+    <Container>
+      <h1>Hello</h1>
+    </Container>
+  )
+}
